7b104b67e5c610e0945be9f2151141e55f7502fa
KAMINWAR SAI PRIYA <k.saipriya08@gmail.com>
2021-08-05 15:10:47 +0530

    first commit

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into user (userid,upassword,uname) values ('priya','priya','Priya');
+insert into user (userid,upassword,uname) values ('aman','aman','Aman');
+insert into user (userid,upassword,uname) values ('madhuri','madhuri','Madhuri');
+insert into user (userid,upassword,uname) values ('kedar','kedar','Kedar');
+
